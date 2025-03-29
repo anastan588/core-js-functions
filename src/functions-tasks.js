c9c56df300d2570200bb7cfad3f9e5658f7b13e4
Anastasiya Andronova <anastasiya5.88@mail.ru>
2025-03-29 22:43:39 +0300

    feat: add task solutions

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -182,8 +182,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...x) => {
+    return fn(...args1, ...x);
+  };
 }
 
 /**
@@ -203,8 +205,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let acc = startFrom;
+  return () => {
+    const result = acc;
+    acc += 1;
+    return result;
+  };
 }
 
 module.exports = {
