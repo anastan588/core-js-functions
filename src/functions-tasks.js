a38aa14ed229fd215849ddfc2b490259b117d726
Anastasiya Andronova <anastasiya5.88@mail.ru>
2025-03-29 23:15:11 +0300

    feat: add task solution

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -119,8 +119,14 @@ function getPolynom(...rest) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let memo;
+  return () => {
+    if (memo === undefined) {
+      memo = func();
+    }
+    return memo;
+  };
 }
 
 /**
