7ec08cb5d9771f3ae56141c66f2d3b1b04dd21e2
Anastasiya Andronova <anastasiya5.88@mail.ru>
2025-03-29 21:21:22 +0300

    feat: add task solutions

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -18,7 +18,7 @@
  *
  */
 function getCurrentFunctionName() {
-  throw new Error('Not implemented');
+  return getCurrentFunctionName.name;
 }
 
 /**
@@ -32,8 +32,12 @@ function getCurrentFunctionName() {
  *   getFunctionBody(hiHello) => "function hiHello() { console.log('hello world'); }"
  *
  */
-function getFunctionBody(/* func */) {
-  throw new Error('Not implemented');
+function getFunctionBody(func) {
+  if (typeof func !== 'function') {
+    return '';
+  }
+  const result = func.toString();
+  return result;
 }
 
 /**
@@ -50,8 +54,11 @@ function getFunctionBody(/* func */) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  const result = funcs.map((func) => {
+    return func.length;
+  });
+  return result;
 }
 
 /**
